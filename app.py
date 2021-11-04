78b44902baa44b29e9b3c52cc1b74e80d343c5a2
sai nivas mangu <sainonbeat99@gmail.com>
2021-11-04 23:50:13 +0530

    added my library, made password to showup in textarea but not properly

--- app.py
+++ app.py
@@ -1,21 +1,28 @@
 from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
+from pglib import myfunctions
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 db = SQLAlchemy(app)
 
+generate = myfunctions.generate
+
 class storePassword(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     password = db.Column(db.String(21), nullable=False)
 
-    def __showPass__(self):
-        return '<Password %r>' % self.password
+    @staticmethod
+    def showPass(length):
+        password = generate(length)
+        return password
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
     if request.method == 'POST':
-        pass
+        l = int(request.form["length"])
+        password = str(storePassword.showPass(l))
+        return render_template('index.html', password=password)
     else:
         return render_template('index.html')
 
